31e18108d01b001b9a50b4630fcaceafb90cef4d
Laila Vinson <vinsonlaila@gmail.com>
2015-05-09 13:40:09 -0700

    added instagram feed

---
+++ js/livefeeds.js
@@ -0,0 +1,17 @@
+// Instafeed: github.com/stevenschobert/instafeed.js
+var feed = new Instafeed({
+    get: 'tagged',
+    tagName: 'springsing2015',
+    clientId: 'bae7ab0c06204012bc64b451d0d5d465'
+});
+feed.run();
+
+
+
+
+!function(d,s,id){
+	var js,fjs=d.getElementsByTagName(s)[0],
+		p=/^http:/.test(d.location)?'http':'https';
+		if(!d.getElementById(id))
+				{js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";
+				fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
